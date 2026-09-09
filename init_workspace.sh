78952e75ea7625944a8fab4048535d26ef6d0bec
dregerj <j.dreger@stat.gov.pl>
2026-09-09 10:05:51 +0000

    init_workspace script

--- init_workspace.sh
+++ init_workspace.sh
@@ -12,4 +12,8 @@ cd $MY_REPO
 
 cd example_code
 
+uv sync
+
+echo "{ \"python.defaultInterpreterPath\": \"./$MY_REPO/example_code/.venv/bin/python\" }" >> ../.vscode/settings.json
+
 mkdir -p TESTOWY
